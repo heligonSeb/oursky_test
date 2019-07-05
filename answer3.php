2604836add16b64456a9e2662effbf6a95d8dc9b
heligonSeb <52566459+heligonSeb@users.noreply.github.com>
2019-07-05 19:43:20 +0200

    answer the question 3 with internet

--- answer3.php
+++ answer3.php
@@ -34,35 +34,48 @@
     function getNextFibNum($int) {
 
         $fibArr = array();   
-        $fibArr[0] = 0; $fibArr[1] = 1; // set initial array keys
-        $i;
+        $fibArr[0] = 0;
+        $fibArr[1] = 1; 
 
-     
-        for ($i=2;$i<=$int+1;$i++) {
-            $fibArr[$i] = $fibArr[$i-1]+$fibArr[$i-2];
+        if($int > 2){
+            for ($i=2;$i<=$int+2;$i++) {
+                $fibArr[$i] = $fibArr[$i-1]+$fibArr[$i-2];
 
-            if ($fibArr[$i] > $int)  
-                return $fibArr[$i];     
-
-        } 
-        
-        if ($fibArr[$i] = $int-1) 
-            return $fibArr[$i-1] + $fibArr[$i-2];
-        
-            
-        if ($fibArr[$i-1] = 1)
-            return $int + $int;    
+                if ($fibArr[$i] > $int)  
+                    return $fibArr[$i];     
     
+            } 
+        } else if($int < 0){ // if $int is a negative number
+
+            $int = abs($int);
+
+            for ($i=2;$i<=$int+2;$i++) {
+                $fibArr[$i] = $fibArr[$i-1]+$fibArr[$i-2];
+
+
+                if ($fibArr[$i] > $int) 
+                    if($i&1){                   // if $i is odd so the value will be positive else is negative
+                        var_dump($i);
+                        return $fibArr[$i];
+                    } else {
+                        var_dump($i);
+                        return -$fibArr[$i];
+                    }    
+            } 
+        } else if( $int == 1 || $int == 2) {
+            return $fibArr[1] + $int;
+        } else {
+            return $fibArr[1];
+        }
     }
 
 
 
 
+
     /****** function test ******/
-    // var_dump(getFibNum(1));
-    // var_dump(getFibNum(9));
-    //var_dump(getFibNum(500));
-    //var_dump(getFibNum(0));
+    // var_dump(getNextFibNum(-4));
+    // var_dump(getNextFibNum(-14));
     var_dump(nextFibonacci([1,22,9]));
     // var_dump(nextFibonacci([1,'22',9]));
     // var_dump(nextFibonacci([1,'a',9]));
