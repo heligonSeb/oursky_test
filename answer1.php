2a7b380bb877e9af71c39ac477d8e299030ba6bc
heligonSeb <52566459+heligonSeb@users.noreply.github.com>
2019-07-05 14:34:30 +0200

    Add files via upload

--- answer1.php
+++ answer1.php
@@ -9,6 +9,9 @@
      */
     function isSubset($array1, $array2){
 
+        if(!is_array($array1) || !is_array($array2))
+            return "the function only work with an array";
+
         $diff = array_diff($array2, $array1); /**** return an array with all value(letter) in array2 is not in the array1 or return an empty array ****/
 
         if(!empty($diff))
@@ -21,6 +24,8 @@
     var_dump(isSubset(["A","B","C","D","E"],["A","E","D"]));
     var_dump(isSubset(["A","B","C","D","E"],["A","D","Z"]));
     var_dump(isSubset(["A","D","E"],["A","A","D","E"]));
+    var_dump(isSubset(["A","D","E"],"not array"));
+    var_dump(isSubset("not array",["A","A","D","E"]));
 
 
 ?>
